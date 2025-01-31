ee37f7a7d31c82bd7dd9e51ff6b30c9785b14841
Sergey <zsv@rtp3.ru>
2025-01-31 19:44:25 +0300

    ArticleParamsForm components

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,7 +4,18 @@ import { Button } from 'src/ui/button';
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
 import { useState } from 'react';
-import { ArticleStateType } from 'src/constants/articleProps';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Text } from 'src/ui/text';
+import { Separator } from 'src/ui/separator';
 
 type ArticleParamsProps = {
 	articleState: ArticleStateType;
@@ -19,6 +30,36 @@ export const ArticleParamsForm = (props: ArticleParamsProps) => {
 			<aside
 				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={props.articleState.fontFamilyOption}
+						title={'шрифт'}
+					/>
+					<RadioGroup
+						name='fontSizeOptions'
+						options={fontSizeOptions}
+						selected={props.articleState.fontSizeOption}
+						title={'размер шрифта'}
+					/>
+					<Select
+						options={fontColors}
+						selected={props.articleState.fontColor}
+						title={'цвет шрифта'}
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={props.articleState.backgroundColor}
+						title={'цвет фона'}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={props.articleState.contentWidth}
+						title={'ширина контента'}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
